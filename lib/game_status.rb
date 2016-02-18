80f004e871831b6aef8dbff79b553cf106174c1e
hlaramelo <hlaramelo@gmail.com>
2016-02-18 14:52:56 +0000

    nitrous-backup at Thu Feb 18 14:52:56 UTC 2016

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,10 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [1,4,8], #diagonal 1
+  [2,4,6] #diagonal 2
+  ]
